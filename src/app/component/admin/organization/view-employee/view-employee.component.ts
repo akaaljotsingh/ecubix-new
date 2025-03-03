2ef807865d363844095a350ecd57dc9bf359cea0
manjodhsingh05 <manjodhs11@gmail.com>
2025-03-03 18:34:01 +0530

    view onboarding

--- src/app/component/admin/organization/view-employee/view-employee.component.ts
+++ src/app/component/admin/organization/view-employee/view-employee.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-view-employee',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './view-employee.component.css'
 })
 export class ViewEmployeeComponent {
-
+   modalRef?: BsModalRef;
+  constructor(private modalService: BsModalService){}
+openCompanyConnectionModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+  }
+  closeModal() {
+    if (this.modalRef) {
+      this.modalRef.hide();
+    }
+  }
 }
